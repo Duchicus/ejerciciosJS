9b15862a8556c8a5e3ac6f9e448ec7fe7229001e
Duchicus <145117344+Duchicus@users.noreply.github.com>
2024-02-27 18:12:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ejerciciosJS
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Duchicus/ejerciciosJS)
---
+++ script.js
@@ -0,0 +1,54 @@
+// Declara una variable llamada "nombre" y asígnale tu nombre
+let nombre = 'Manel';
+// Declara una variable llamada "edad" y asígnale tu edad
+let edad = 22;
+// Declara una variable llamada "ciudad" y asígnale el nombre de tu ciudad
+let ciudad = 'Valencia';
+// Declara dos variables numéricas y realiza una suma
+let num1 = 1;
+let num2 = 2;
+
+let result = num1 + num2;
+console.log(result);
+// Declara una variable numérica y otra de texto, y concaténalas
+let numero = 2;
+let texto = 'hola';
+
+let concatenacion = numero + texto;
+console.log(concatenacion);
+// Declara una variable y asígnale un valor inicial
+let valor = 1;
+// Incrementa el valor de la variable en 1 y muestra el resultado
+valor++;
+console.log(valor);
+// Cambia el valor de la variable a otro número y muestra el resultado
+valor = 2;
+console.log(valor);
+// Crea un array de números
+let arraynumeros = [1, 2, 3];
+// Crea un array de strings
+let arraystrings = ['1', '2', '3'];
+// Crea un array mixto
+let arraymixto = [1, '2', 3];
+// Accede al primer elemento del array "numeros"
+console.log(arraynumeros);
+// Accede al tercer elemento del array "frutas"
+console.log(arraystrings);
+// Modifica el segundo elemento del array "numeros"
+arraynumeros[1] = 4;
+console.log(arraynumeros);
+// Modifica el último elemento del array "frutas"
+arraystrings[2] = '4';
+console.log(arraystrings);
+// Agrega un nuevo elemento al final del array "numeros"
+arraynumeros.push(10);
+console.log(arraynumeros);
+// Agrega un nuevo elemento al principio del array "frutas"
+arraystrings.unshift('0');
+console.log(arraystrings);
+// Elimina el último elemento del array "numeros"
+arraynumeros.pop();
+console.log(arraynumeros);
+// Elimina el primer elemento del array "frutas"
+arraystrings.shift();
+console.log(arraystrings);
